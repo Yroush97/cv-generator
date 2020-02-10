26b729c49800fb245c61c181728431c0bdad56af
Charlie <47629661+Yroush97@users.noreply.github.com>
2020-02-10 13:24:54 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <generator/>
+  </div>
+</template>
+
+<script>
+import generator from "./components/generator.vue";
+
+export default {
+  name: "app",
+  components: {
+    generator
+  }
+};
+</script>
+
+<style lang="stylus">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
